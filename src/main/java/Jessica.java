d8b1e6081672d6153c675bc27f1bd9d63fbf8e4e
nhocmt227 <phong1572005@gmail.com>
2025-01-20 15:10:54 +0800

    Implement storing text feature - level 2

--- src/main/java/Jessica.java
+++ src/main/java/Jessica.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Jessica {
@@ -10,9 +13,25 @@ public class Jessica {
                 + "    ____________________________________________________________\n";
     }
 
+    public static String decorateListOfInput(List<String> list) {
+        String result = "    ____________________________________________________________\n";
+        for (int i = 0; i < list.size(); i++) {
+            String temp = "     " + (i + 1) + ". " + list.get(i) + "\n";
+            result += temp;
+        }
+        result += "    ____________________________________________________________\n";
+        return result;
+    }
+
     public static boolean detectBye(String input) {
         return input.equals("bye") || input.equals("Bye");
     }
+
+    public static boolean detectList(String input) {
+        return input.equals("list") || input.equals("List");
+    }
+
+    private static List<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
@@ -31,7 +50,12 @@ public class Jessica {
             if (detectBye(input)) {
                 break;
             }
-            System.out.println(decorateInput(input));
+            if (detectList(input)) {
+                System.out.println(decorateListOfInput(list));
+            } else {
+                list.add(input);
+                System.out.println(decorateInput("added: " + input));
+            }
         }
         System.out.println(decorateInput("Bye. Hope to see you again soon!"));
     }
